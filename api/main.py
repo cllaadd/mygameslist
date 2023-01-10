70bbe679e001b85c5913867eef0a8f400d287ce1
Claudia Harrison <claudia3695@gmail.com>
2023-01-10 15:48:47 -0600

    still working on fixing account model

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import games, accounts
 from routers.authenticator import authenticator
+from routers import games, accounts
 import os
 
 
