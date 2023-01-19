40ec699076dbfcad8982ac413b5a6fa2bb34f92f
alephsoup <brianpatrickeggert@gmail.com>
2023-01-19 16:13:25 -0600

    modified account.js and index.js

--- ghi/src/Account.js
+++ ghi/src/Account.js
@@ -5,11 +5,25 @@ function UserComponent() {
   const [username, setUsername] = useState("");
 
   return (
-    <div class="card">
-      <img src="https://www.w3schools.com/howto/img_avatar.png" />
-      <h1>
-        <b>{username}</b>
-      </h1>
+    <div className="container d-flex justify-content-center align-items-center">
+      <div className="card">
+        <div className="image">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            className="img-fluid"
+          />
+        </div>
+        <div className="mt-5 text-center">
+          <h4 className="mb-0">${username}</h4>
+          <button className="btn btn-primary btn-sm view list">
+            username's games list
+          </button>
+          <div className="games">
+            <h6 className="mb-0">Games</h6>
+            <span>50</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
