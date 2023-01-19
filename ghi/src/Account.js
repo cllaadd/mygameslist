51756635465b35eb0d40ff5a229e34edd26f3641
alephsoup <brianpatrickeggert@gmail.com>
2023-01-19 15:25:35 -0600

    trying to reconcile divergent branches

--- ghi/src/Account.js
+++ ghi/src/Account.js
@@ -5,13 +5,25 @@ function UserComponent() {
   const [username, setUsername] = useState("");
 
   return (
-    <div class="card">
-      <img src="https://www.w3schools.com/howto/img_avatar.png" />
-      <h1>
-        <b>{username}</b>
-      </h1>
+    <div class="container d-flex justify-content-center align-items-center">
+      <div class="card">
+        <div class="image">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            class="img-fluid"
+          />
+        </div>
+        <div class="mt-5 text-center">
+          <h4 class="mb-0">username</h4>
+          <button class="btn btn-primary btn-sm view list">
+            username's games list
+          </button>
+          <div class="games">
+            <h6 class="mb-0">Games</h6>
+            <span>50</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default UserComponent;
