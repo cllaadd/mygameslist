45d10db09a0ce3d148144ba44002b6ee6e3152b0
cmike1775 <102187519+cmike1775@users.noreply.github.com>
2023-01-19 15:45:17 -0600

    Added bootstrap to nav,main,login,sighup

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -4,6 +4,8 @@ import { AuthProvider, useToken } from "./Auth";
 import LoginComponent from './Login';
 import LogoutComponent from './Logout';
 import SignupComponent from './Signup';
+import MainPageComponent from './MainPage';
+import Nav from './Nav';
 
 function GetToken() {
     // Get token from JWT cookie (if already logged in)
@@ -14,15 +16,18 @@ function GetToken() {
 function App() {
   return (
     <BrowserRouter>
+    <Nav/>
         <AuthProvider>
             <GetToken />
                 <Routes>
                   <Route path = "/login" element={<LoginComponent />} ></Route>
                   <Route path = "/logout" element={<LogoutComponent />} ></Route>
                   <Route path = "/signup" element={<SignupComponent />} ></Route>
+                  <Route path = "/MainPage" element={<MainPageComponent />} ></Route>
                 </Routes>
         </AuthProvider>
     </BrowserRouter>
+
   );
 }
 
--- ghi/src/Signup.js
+++ ghi/src/Signup.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useToken } from "./Auth";
 import { useNavigate } from "react-router-dom";
+import controller from "./images/controller.png";
 
 function SignupComponent() {
     const [account, setAccount] = useState({
@@ -42,16 +43,19 @@ function SignupComponent() {
             console.error("Passwords do not match");
         }
     };
+return(
+  <div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
 
 
-  return (
-    <div className="row">
-      <div className="offset-3 col-6">
-        <div className="shadow p-4 mt-4">
-          <h1>Sign Up</h1>
+          <div class="card-body p-md-5">
+            <div class="row justify-content-center">
+          <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+              <h2>Sign Up</h2>
           <form onSubmit={handleSubmit} id="signup-form">
-            <div className="form-floating mb-3">
-              <input
+          <div className="form-floating mb-3">
+             <input
                 onChange={handleChange}
                 value={account.username}
                 placeholder="Enter your username"
@@ -91,10 +95,78 @@ function SignupComponent() {
             </div>
             <button className="btn btn-primary">Sign up</button>
           </form>
+            </div>
+              <div class="col-md-10 col-lg-6 col-xl-7  align-items-center order-1 order-lg-2">
+
+              <img class="img-fluid width=50% height=50%" src={controller} alt="video game controller" />
+
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+
+
+)
+
+
 }
 
 export default SignupComponent;
+  // return (
+  //   <div className="container h-100">
+  //   <div className="row d-flex justify-content-center align-items-center h-100">
+  //   <div className="card-body p-md-5">
+  //   <div className="row ">
+  //     <div className="offset-3 col-6">
+  //       <div className="shadow p-4 mt-4">
+  //         <h1>Sign Up</h1>
+  //         <form onSubmit={handleSubmit} id="signup-form">
+  //           <div className="form-floating mb-3">
+  //             <input
+  //               onChange={handleChange}
+  //               value={account.username}
+  //               placeholder="Enter your username"
+  //               required
+  //               type="text"
+  //               name="username"
+  //               id="username"
+  //               className="form-control"
+  //             />
+  //             <label htmlFor="username">Username</label>
+  //           </div>
+  //           <div className="form-floating mb-3">
+  //             <input
+  //               onChange={handleChange}
+  //               value={account.password}
+  //               placeholder="Enter your password"
+  //               required
+  //               type="password"
+  //               name="password"
+  //               id="password"
+  //               className="form-control"
+  //             />
+  //             <label htmlFor="password">Password</label>
+  //           </div>
+  //           <div className="form-floating mb-3">
+  //             <input
+  //               onChange={handleChange}
+  //               value={account.passwordConfirm}
+  //               placeholder="Confirm password"
+  //               required
+  //               type="passwordConfirm"
+  //               name="passwordConfirm"
+  //               id="passwordConfirm"
+  //               className="form-control"
+  //             />
+  //             <label htmlFor="passwordConfirm">Confirm Password</label>
+  //           </div>
+  //           <button className="btn btn-primary">Sign up</button>
+  //         </form>
+  //       </div>
+  //     </div>
+  //   </div>
+  //   </div>
+  //   </div>
+  //   </div>
+  // );
