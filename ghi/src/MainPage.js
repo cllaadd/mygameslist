8a094421fa44b7ec024b2930ad4bb05435c838aa
cmike1775 <102187519+cmike1775@users.noreply.github.com>
2023-01-19 18:11:08 -0600

    added more to front end. nav bar incomplete

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -6,7 +6,7 @@ function MainPageComponent() {
       <h1 className="display-5 fw-bold">MyGamesList</h1>
       <div className="col-lg-6 mx-auto">
         <p className="lead mb-4">
-           <img class="img-fluid width=50% height=50%" src={controller} alt="video game controller" />
+           <img class="img-fluid " src={controller} alt="video game controller" />
 
         </p>
       </div>
--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -1,17 +1,28 @@
 import {Link, NavLink } from "react-router-dom";
+import littlecontroller from "./images/littlecontroller.png";
 
 function Nav() {
   return (
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light background-color:#7950f2">
   <a class="navbar-brand" href="#"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <NavLink className="navbar-brand" to="/mainpage">
+        <img class="img-fluid width=50% height=50%" src={littlecontroller} alt="controller pic" />
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Link</a>
