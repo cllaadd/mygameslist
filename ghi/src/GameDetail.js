0af7a876849932359eb408d4b1ea939485e288a5
Garrett <gstrand99@gmail.com>
2023-01-23 02:55:23 -0600

    finished more of the game detail page

--- ghi/src/GameDetail.js
+++ ghi/src/GameDetail.js
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import { useToken } from "./Auth";
 import { useParams, Link } from "react-router-dom";
 import './GameDetail.css';
+import $ from 'jquery';
+import Carousel from "./Carousel"
+
+
 
 const CircularProgressBar = (props) => {
   const sqSize = props.sqSize;
@@ -44,12 +48,14 @@ const CircularProgressBar = (props) => {
   );
 }
 
+
 const GameDetail = () => {
     const [games, setGame] = useState([])
     const [refresh, setRefresh] = useState(false)
     const [showMore, setShowMore] = useState(false);
     const [rating, setRating] = useState(false);
     const {id} = useParams()
+
 
 
     useEffect(() => {
@@ -69,13 +75,16 @@ const GameDetail = () => {
     }
     loadData()
 }, [id, refresh, rating])
+
   return (
     <div>
     <div className="main-body">
+
         {games?.map(game => {
             return (
                 <div>
-                    <div className="game-header-container">
+
+                     <div className="game-header-container">
                         <div className="game-cover-container">
                                 <img className="game-cover" src={game.cover}/>
                         </div>
@@ -112,6 +121,28 @@ const GameDetail = () => {
                         </div>
                     </div>
 
+                    <h4>Screenshots</h4>
+                    <div className='screenshots-carousel' style={{maxWidth: 1024}}>
+                        <Carousel>
+                            {game.screenshots.map((screenshot, index) => (
+                                <img src={screenshot} key={index} style={{height: "576px", width: "1024px"}}/>
+                            ))}
+                        </Carousel>
+                    </div>
+
+                    <h4>Similar Games</h4>
+                    <div className='similar-game-carousel' style={{maxWidth: 420}}>
+                        <Carousel>
+                            {game.similar_games_id.map((similarGame, index) => (
+                                <div>
+                                    <img src={similarGame.cover} className="cover_art" alt={similarGame.name} />
+                                    <Link to={`/games/${similarGame.id}`} onClick={() => setRefresh(true)}>
+                                        <h3>{similarGame.name}</h3>
+                                    </Link>
+                                </div>
+                            ))}
+                        </Carousel>
+                    </div>
 
                     <h4>Alternative Names</h4>
                     {Object.values(game.alternative_names).slice(0, showMore ? undefined : 5).join(', ')}
@@ -120,34 +151,41 @@ const GameDetail = () => {
                         {showMore ? 'Show Less' : 'Show More'}
                         </button>
                     )}
+
                     <h4>Category</h4>
                     <body>{game.category}</body>
+
                     <h4>Collection</h4>
                     {game.collection_id.map((collection, index) => (
                         <div key={index}>
                             <body>{collection.name}</body>
                         </div>
                         ))}
+
                     <h4>DLCS</h4>
                     <body>{game.dlcs_id}</body>
+
                     <h4>Franchises</h4>
                     {game.franchises_id.map((franchise, index) => (
                         <div key={index}>
                             <body>{franchise.name}</body>
                         </div>
                         ))}
+
                     <h4>Game Modes</h4>
                     {game.game_modes_id.map((game_mode, index) => (
                         <div key={index}>
                             <body>{game_mode.name}</body>
                         </div>
                         ))}
+
                     <h4>Genres</h4>
                     {game.genres_id.map((genre, index) => (
                         <div key={index}>
                             <body>{genre.name}</body>
                         </div>
                         ))}
+
                     <h4>Involved Companies</h4>
                     {game.involved_companies_id.map((company, index) => (
                         <div key={index}>
@@ -155,6 +193,7 @@ const GameDetail = () => {
                             <img src={company.logo} className="img-responsive logo_med logo_med" />
                         </div>
                         ))}
+
                     <h4>Keywords</h4>
                     <body>
                         {Object.values(game.keywords_id).slice(0, showMore ? undefined : 10).join(', ')}
@@ -162,69 +201,68 @@ const GameDetail = () => {
                         <button onClick={() => setShowMore(!showMore)}>
                     {showMore ? 'Show Less' : 'Show More'}
                     </button>
+
                     <h4>Platforms</h4>
                     {game.platforms_id.map((platform, index) => (
                         <div key={index}>
                             <body>{platform.name}</body>
                         </div>
                         ))}
+
                     <h4>Player Perspectives</h4>
                     {game.player_perspectives_id.map((perspective, index) => (
                         <div key={index}>
                             <body>{perspective.name}</body>
                         </div>
                         ))}
+
                     <h4>Ports</h4>
                     <body>{game.ports_id}</body>
+
                     <h4>Remakes</h4>
                     <body>{game.remakes_id}</body>
+
                     <h4>Remasters</h4>
                     <body>{game.remasters_id}</body>
-                    <h4>Similar Games</h4>
-                    <table>
-                    <tbody>
-                        {game.similar_games_id.map((similarGame, index) => (
-                        <tr key={index}>
-                            <td><img src={similarGame.cover} className="img-fluid" style={{width: '132px', height: '187px'}}/></td>
-                            <td>
-                            <Link to={`/games/${similarGame.id}`} onClick={() => setRefresh(true)}>
-                                <h3>{similarGame.name}</h3>
-                            </Link>
-                            </td>
-                        </tr>
-                        ))}
-                    </tbody>
-                    </table>
+
                     <h4>Summary</h4>
                     <body>{game.summary}</body>
+
                     <h4>Storyline</h4>
                     <body>{game.storyline}</body>
+
                     <h4>Status</h4>
                     <body>{game.status}</body>
-                    <h4>Screenshots</h4>
+
+                    {/* <h4>Screenshots</h4>
                     <div style={{display: "flex", flexWrap: "nowrap", overflowX: "scroll"}}>
                     {game.screenshots.map((screenshot, index) => (
                         <img src={screenshot} key={index} className="img-fluid" style={{height: "288px", width: "512px"}}/>
                     ))}
-                    </div>
+                    </div> */}
+
                     <h4>Themes</h4>
                     {game.themes_id.map((theme, index) => (
                         <div key={index}>
                             <body>{theme.name}</body>
                         </div>
                         ))}
+
                     <h4>Total Rating Count</h4>
                     <body>{game.total_rating_count}</body>
+
                     <h4>Version Parent</h4>
                     <body>{game.version_parent_id}</body>
+
                     <h4>Version Title</h4>
                     <body>{game.version_title}</body>
                 </div>
 
-            );
-        })}
-    </div>
 
+
+
+    );})}
+    </div>
     </div>
   );
 }
