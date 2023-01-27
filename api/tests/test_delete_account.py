c82f4feef73a47e168fe1af6943776c5d12c3ac1
alephsoup <brianpatrickeggert@gmail.com>
2023-01-27 15:40:18 -0600

    delete account test and changes to accounts page

---
+++ api/tests/test_delete_account.py
@@ -0,0 +1,27 @@
+from fastapi.testclient import TestClient
+from queries.accounts import AccountQueries
+from main import app
+
+
+client = TestClient(app)
+
+
+
+class AccountQueriesMock:
+    def delete(self, account_id):
+        return [{
+            "id" : "1",
+            "username": "chris",
+            "password": "password"
+        }]
+
+def test_delete_account():
+        #arrange
+        app.dependency_overrides[AccountQueries] = AccountQueriesMock
+        #act
+        response = client.delete("/api/accounts/1")
+        #assert
+        assert response.status_code == 200
+        assert response.json()== True
+
+        app.dependency_overrides = {}
